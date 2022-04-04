b2af7f22c9b634797ef0362c89c03cbadf6c9f98
Donald189 <donaldkarey@gmail.com>
2022-04-04 20:03:37 +0530

    Welcome to Adv AddressBook System

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AdvAddressBookDB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Advance AddressBook problem");
+
+            //AddressBookRepo addressBookRepo = new AddressBookRepo();
+            //AddressBookModel addressBookModel = new AddressBookModel();
+            //addressBookModel.FirstName = "Donald";
+            //addressBookModel.LastName = "JK";
+            //addressBookModel.Address = "Muthammal Colony";
+            //addressBookModel.City = "Thoothukudi";
+            //addressBookModel.State = "TamilNadu";
+            //addressBookModel.Zip = 628002;
+            //addressBookModel.PhoneNumber = 8056446118;
+            //addressBookModel.Email = "donaldkarey@gmail.com";
+            //addressBookModel.AddressBookName = "Self address book";
+            //addressBookModel.AddressBookType = "Self";
+            //addressBookRepo.DataBaseConnection();
+            //addressBookRepo.addNewContactToDataBase(addressBookModel);
+            //addressBookRepo.EditExiContactToDataBase(addressBookModel, "Dhoni");
+            //addressBookRepo.deleteExiContactInDataBase("Donald");
+            //addressBookRepo.personBelongingCityOrState();
+        }
+    }
+}
